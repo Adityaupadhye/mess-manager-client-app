6b42490e562b6056d54ca33d88365278bea1d55b
aasim <mohammadaasim61.ma@gmail.com>
2024-11-09 11:04:03 +0530

    All working uptill planned

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/auth/login.service';
 import { Router } from '@angular/router';
 import { MenuService } from '../../services/menu/menu.service';
+import { SyncService } from '../../services/sync/sync.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,13 +11,18 @@ import { MenuService } from '../../services/menu/menu.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   constructor(
     public loginService: LoginService,
     public menuService: MenuService,
-    public router: Router
+    public router: Router,
+    private syncService: SyncService,
+    
   ) {
 
+  }
+  ngOnInit(): void {
+   
   }
 
   addWastage() {
@@ -28,4 +34,31 @@ export class NavbarComponent {
     this.menuService.isAddingMenu = true; 
     this.menuService.isAddingWastage = false;
   }
+
+  isScanPage(): boolean {
+    return this.router.url.includes('scan');
+  }
+
+  syncdata(){
+    this.syncService.syncLogEntries();
+  }
+
+
+  // syncdata() {
+  //   this.syncService.syncLogEntries().subscribe({
+  //     next: () => {
+  //       this.showToast('Sync completed successfully!', 'success');
+  //     },
+  //     error: () => {
+  //       this.showToast('Sync failed. Please try again.', 'error');
+  //     }
+  //   });
+  // }
+
+  // showToast(message: string, type: 'success' | 'error') {
+  //   this.snackBar.open(message, 'Close', {
+  //     duration: 3000,
+  //     panelClass: type === 'success' ? 'snack-bar-success' : 'snack-bar-error'
+  //   });
+  // }
 }
