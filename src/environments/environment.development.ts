8d220f0780f4ff5274604e77f91c90efe6270352
Sanchit Kadwe <sanchitkadwea@gmail.com>
2025-11-19 11:23:03 +0530

    Initial Commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://192.168.1.104:8000/'
-    // apiBaseUrl: 'http://127.0.0.1:8000/'
+    // apiBaseUrl: 'http://192.168.1.104:8000/'
+    apiBaseUrl: 'http://127.0.0.1:8000/'
 };
